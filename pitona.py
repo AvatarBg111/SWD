3024561b031001c94dab76765745c62eeaeddae2
AvatarBg111 <vasivk555@gmail.com>
2024-03-29 12:17:55 +0200

    Upload pic feature to feature branch

---
+++ pitona.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def visualize_image(image_path):
+    # Load the image
+    img = mpimg.imread(image_path)
+    
+    # Display the image
+    plt.imshow(img)
+    plt.axis('off')  # Turn off axis
+    plt.show()
+
+# Example usage:
+image_path = "example_image.jpg"  # Change this to the path of your image
+visualize_image(image_path)
